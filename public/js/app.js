e5ff96a5dc7cfc1778c8f198a76012e92e15547d
Shivam Kumar <shivam.saha06@gmail.com>
2020-07-22 12:50:42 +0530

    Project Complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,26 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 
-console.log("client side java script is loaded")
+weatherForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    const location = search.value 
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=> {
     response.json().then((data) => {
-        console.log(data)
+        if(data.error){
+            messageOne.textContent = data.error
+        }
+        else{
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
     }) 
 })
 
-fetch('http://localhost:3000/weather?address=').then((response)=> {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }
-        else{
-        console.log(data.location)
-        console.log(data.forecast)
-        }
-    }) 
 })
